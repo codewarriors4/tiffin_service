6e09c697f7ffa17ac8d3d00dbb295fa1a25aa339
Jaison <jaison.joseph26@gmail.com>
2018-06-12 11:14:47 -0400

    added usertype to be saved while registration

--- web/app/User.php
+++ web/app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','token','isEmailVerified'
+        'name', 'email', 'password','token','isEmailVerified','isActive','UserFname','UserLname','UserType','UserPhone','UserCountry','UserProvince','UserCity','UserZipCode','UserCompanyName','isBlocked','remember_token'
     ];
 
     /**
