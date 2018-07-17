8c0ae2cc37426a16755e0c6f8fc6003c46320368
Jaison <jaison.joseph26@gmail.com>
2018-07-17 06:03:03 -0400

    saving card number in payment table

--- web/app/Http/Controllers/Api/PaymentController.php
+++ web/app/Http/Controllers/Api/PaymentController.php
@@ -134,7 +134,8 @@ class PaymentController extends Controller
                 $expiration_year = "20".request('expiration_year');
                 $homemaker_id = request('HomeMakerId');
                 $expiration_month = request('expiration_month');
-                $card_number = 'XXXX-XXXX-XXXX-'.substr(request('PCardNumber'),-4);
+                $lastfourdig = substr(request('PCardNumber'),-4)
+                $card_number = 'XXXX-XXXX-XXXX-'.$lastfourdig;
 
                 $card_number = $this->ccMasking($card_number);
 
