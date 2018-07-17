73f31065a59de4f68768aebcc6a89c40203d9362
Jaison <jaison.joseph26@gmail.com>
2018-07-17 06:49:57 -0400

    push notification on payment in progress

--- web/app/Http/Controllers/Api/PaymentController.php
+++ web/app/Http/Controllers/Api/PaymentController.php
@@ -2,21 +2,15 @@
 
 namespace TiffinService\Http\Controllers\Api;
 
-use TiffinService\Http\Requests\CreditCardRequest;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Mail\Mailer;
 use TiffinService\HomeMaker;
 use TiffinService\Http\Controllers\Controller;
-use LVR\CreditCard\CardNumber;
-use LVR\CreditCard\Cards\Visa;
-use LVR\CreditCard\ExpirationDateValidator;
 use TiffinService\Http\Requests\CardValidator;
 use TiffinService\Payment;
-use TiffinService\SubscribedPackages;
 use TiffinService\Subscription;
-use Illuminate\Mail\Mailer;
-use Carbon\Carbon;
 use TiffinService\TiffinSeeker;
-
 
 class PaymentController extends Controller
 {
@@ -49,24 +43,23 @@ class PaymentController extends Controller
 
     //}*/
 
+    //send sample response in JSON as below for the following method
+    /*    {
+    "cart":[
+    {
+    "package_id":2
 
- //send sample response in JSON as below for the following method
-/*    {  
-   "cart":[  
-      {  
-         "package_id":2
-         
-      },
-      {  
-         "package_id":3
-    
-      },
-      {  
-          "package_id":40
-     
-      }
-   ]
-}*/
+    },
+    {
+    "package_id":3
+
+    },
+    {
+    "package_id":40
+
+    }
+    ]
+    }*/
 
     private $mailer;
 
@@ -76,6 +69,7 @@ class PaymentController extends Controller
     }
 
     public function CartSummary(Request $request) // shows the cart summary
+
     {
 
         $cart = request('cart');
@@ -106,121 +100,140 @@ class PaymentController extends Controller
 
                 }
 
-
-
             }
 
-            return response()->json(['packages_array'=>$packages_array],200);
+            return response()->json(['packages_array' => $packages_array], 200);
 
         } catch (Exception $e) {
 
-
-        		    return response()->json(['status'=>'failed'],203);
-
+            return response()->json(['status' => 'failed'], 203);
 
         }
 
     }
 
-    public function ccMasking($number, $maskingCharacter = 'X') {
-    return substr($number, 0, 4) . str_repeat($maskingCharacter, strlen($number) - 8) . substr($number, -4);
-}
+    public function Payment(CardValidator $request) // shows the cart summary
 
-     public function Payment(CardValidator $request) // shows the cart summary     
-    {   
+    {
 
         try {
 
-                $expiration_year = "20".request('expiration_year');
-                $homemaker_id = request('HomeMakerId');
-                $expiration_month = request('expiration_month');
-                $lastfourdig = substr(request('card_number'),-4);
-                $card_number = 'XXXX-XXXX-XXXX-'.$lastfourdig;
+            $expiration_year  = "20" . request('expiration_year');
+            $homemaker_id     = request('HomeMakerId');
+            $expiration_month = request('expiration_month');
+            $lastfourdig      = substr(request('card_number'), -4);
+            $card_number      = 'XXXX-XXXX-XXXX-' . $lastfourdig;
+
+            $cvc           = request('cvc');
+            $cost          = request('subtotal');
+            $hst           = request('hst');
+            $total_cost    = request('total');
+            $subsPackageId = request('HMPid');
+
+            $dt = Carbon::now();
+
+            $subscription_start_date = $dt;
+            $subscription_end_date   = Carbon::now()->addDays(30);
+            $substatus               = 0;
+
+            if ($dt->year > $expiration_year) {
+
+                return response()->json(['error' => 'the year must be greater or equal touch(filename) current year'], 203);
+
+            }
+
+            if ($dt->year == $expiration_year && $dt->month > $expiration_month) {
+
+                return response()->json(['error' => 'please check the card expiry month'], 203);
+
+            }
+
+            $tsId = TiffinSeeker::where("UserId", \Auth::user()->id)->first();
+
+            $subscription                 = new Subscription;
+            $subscription->SubCost        = $total_cost;
+            $subscription->SubStartDate   = $subscription_start_date;
+            $subscription->SubEndDate     = $subscription_end_date;
+            $subscription->SubStatus      = $substatus;
+            $subscription->TiffinSeekerId = $tsId->TSId;
+            $subscription->HomeMakerId    = $homemaker_id;
+            $subscription->HMPid          = $subsPackageId;
+            $subscription->save();
+
+            $payment = new Payment;
+
+            $payment->PAmt        = $total_cost;
+            $payment->PTax        = $hst;
+            $payment->PSubTotal   = $cost;
+            $payment->PCardNumber = $card_number;
+
+            $payment->PStatus = '1';
+
+            $payment->SubscID = $subscription->SubId;
+            $payment->save();
+
+            Subscription::where('SubId', $subscription->SubId)->update(['SubStatus' => 1]);
+
+            $user_details = User::join('homemaker', 'homemaker.UserId', '=', 'users.id')->where('homemaker.HMId', $homemaker_id)->first();
 
 
-                $cvc = request('cvc');
-                $cost = request('subtotal');
-                $hst = request('hst');
-                $total_cost = request('total');
-                $subsPackageId = request('HMPid');
 
+            $fcmtokens = UserMobInfo::where('userID', $user_details->id)->select("fcmtoken")->get();
 
-           
-                $dt = Carbon::now();
+            $device_token_array = array();
 
-                $subscription_start_date = $dt;
-                $subscription_end_date = Carbon::now()->addDays(30); 
-                $substatus = 0;        
+            if ($fcmtokens->count() > 0) {
 
-                  if($dt->year > $expiration_year){
+                foreach ($fcmtokens as $key => $token) {
+                    array_push($device_token_array, $token->fcmtoken);
+                }
 
-                     return response()->json(['error'=>'the year must be greater or equal touch(filename) current year'],203);
+                $title   = "You have got a new Subscription "
+                $body    = \Auth::user()->UserFname." subscribed to one of your packages !";
+                $feedbck = \PushNotification::setService('fcm')
+                    ->setMessage([
+                        'notification' => [
+                            'title' => $title,
+                            'body'  => $body,
+                            'sound' => 'default',
+                        ],
+                        'data'         => [
+                            'extraPayLoad1' => 'value1',
+                            'extraPayLoad2' => 'value2',
+                        ],
+                    ])
+                    ->setApiKey('AAAAoeIud7w:APA91bGsANVi6YE_HfJOODY6nwnBVCLWMx4Suinb6tux6R6jePDA-qX2mpNcGanlEQusyqnZ1PaZjFePkDDla6PUxgF1KZVm3WTPdbq7wYGfY9LPidiHEPCbTtQFT89bDMs5GotOb63cNYll-RG1Kd7OFJE47T1I7w')
+                    ->setDevicesToken($device_token_array)
+                    ->send()
+                    ->getFeedback();
 
-                  }
+                dd($feedbck);
 
+            }
 
-                 if($dt->year == $expiration_year && $dt->month > $expiration_month){
+            /*                if (count($cart) > 0) {
 
-                     return response()->json(['error'=>'please check the card expiry month'],203);
+            foreach ($cart as $key => $value) {
 
-                  }
+            $add_package = new SubscribedPackages;
+            $add_package->PackageId = $value;
+            $add_package->SubscrpId = $subscription->SubId;
+            $add_package->save();
+            }
 
-                  $tsId = TiffinSeeker::where("UserId",\Auth::user()->id)->first();
+            }*/
 
-                  $subscription = new Subscription;
-                  $subscription->SubCost = $total_cost;
-                  $subscription->SubStartDate = $subscription_start_date;
-                  $subscription->SubEndDate = $subscription_end_date;
-                  $subscription->SubStatus = $substatus;
-                  $subscription->TiffinSeekerId = $tsId->TSId;
-                  $subscription->HomeMakerId = $homemaker_id;
-                  $subscription->HMPid = $subsPackageId;
-                  $subscription->save();
-             
-                  $payment = new Payment;
+            /*           Mail::to(\Auth::user()->email)->send();
 
-                  $payment->PAmt = $total_cost;
-                  $payment->PTax = $hst;
-                  $payment->PSubTotal = $cost;
-                  $payment->PCardNumber = $card_number;
+            $message = "Payment complete";
+            mail('\Auth::user()->email', 'Payment complete', $message);*/
+            return response()->json(['status' => 'success'], 200);
 
-                  $payment->PStatus = '1';
-
-                  $payment->SubscID = $subscription->SubId;
-                  $payment->save();
-
-
-
-                  Subscription::where('SubId',$subscription->SubId)->update(['SubStatus' => 1]);
-
-  /*                if (count($cart) > 0) {
-
-                    foreach ($cart as $key => $value) {
-
-                        $add_package = new SubscribedPackages;
-                        $add_package->PackageId = $value;
-                        $add_package->SubscrpId = $subscription->SubId;
-                        $add_package->save();
-                    }
-
-                  }*/
-
-       /*           Mail::to(\Auth::user()->email)->send();
-                  
-                  $message = "Payment complete";
-                  mail('\Auth::user()->email', 'Payment complete', $message);*/
-                 return response()->json(['status'=>'success'],200); 
-            
         } catch (Exception $e) {
 
-
-             return response()->json(['status'=>'failed'],203); 
+            return response()->json(['status' => 'failed'], 203);
         }
 
-
-           
-
-
- 	}
+    }
 
 }
