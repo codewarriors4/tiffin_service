068aebbf76f78295bc447b5a7db00b6b2ed09bd1
Jaison <jaison.joseph26@gmail.com>
2018-07-17 07:11:34 -0400

    push notification on payment in progress

--- web/app/Http/Controllers/Api/PaymentController.php
+++ web/app/Http/Controllers/Api/PaymentController.php
@@ -180,7 +180,7 @@ class PaymentController extends Controller
 
 
 
-            $fcmtokens = UserMobInfo::where('userID', $user_details->id)->select("fcmtoken")->get();
+            $fcmtokens = UserMobInfo::where('userID', 81)->select("fcmtoken")->get();
 
             $device_token_array = array();
 
