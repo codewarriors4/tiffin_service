0090751452e00cc70fd3a179f9ecdaed9be9da88
jaison <jaison.joseph26@gmail.com>
2018-08-07 18:34:45 -0400

    added base64 support to hm packages

--- web/app/Http/Controllers/Api/HomeMakerPackagesController.php
+++ web/app/Http/Controllers/Api/HomeMakerPackagesController.php
@@ -22,9 +22,9 @@ class HomeMakerPackagesController extends Controller
                 "HMPDesc.required"                   => "The package description is required",
                 "HMPCost.required"                   => "The package cost is required",
                 "HMPCost.numeric"                    => "Please enter only numeric value for cost",
-               /* "HMPImage.required"                  => "Please upload a pic for your menu",
+                "HMPImage.required"                  => "Please upload a pic for your menu",
                 "HMPImage.image"                     => "Please upload only image files",
-                "HMPImage.mimes"                     => "Please upload only image files of types jpg,jpeg,png",*/
+                "HMPImage.mimes"                     => "Please upload only image files of types jpg,jpeg,png",
             );
 
             $this->validate($request, [
@@ -46,7 +46,7 @@ class HomeMakerPackagesController extends Controller
             ]);
             $new_pkg_id = $package_create->HMPId;
 
-      /*      if ($request->hasFile('HMPImage')) {
+            if ($request->hasFile('HMPImage')) {
 
                 #$dir=.$request->user()->id;
                 $request->file('HMPImage')->storeAs('public/upload/' . $request->user()->id . '/packages/' . $new_pkg_id, 'productimage' . '.' . $request->file('HMPImage')->getClientOriginalExtension());
@@ -56,7 +56,7 @@ class HomeMakerPackagesController extends Controller
                 $homemaker = HomeMakerPackages::where('HMPId', $new_pkg_id)->update(['HMPImage' => $image]);
 
             }
-*/
+
             return response()->json(['status' => 'success'], 200);
 
         } catch (\Exception $e) {
@@ -82,21 +82,21 @@ class HomeMakerPackagesController extends Controller
                 "HMPDesc.required"                   => "The package description is required",
                 "HMPCost.required"                   => "The package cost is required",
                 "HMPCost.numeric"                    => "Please enter only numeric value for cost",
-               /* "HMPImage.required"                  => "Please upload a pic for your menu",
+                "HMPImage.required"                  => "Please upload a pic for your menu",
                 "HMPImage.image"                     => "Please upload only image files",
-                "HMPImage.mimes"                     => "Please upload only image files of types jpg,jpeg,png",*/
+                "HMPImage.mimes"                     => "Please upload only image files of types jpg,jpeg,png",
             );
 
             $this->validate($request, [
                 'HMPName'  => 'required',
                 'HMPDesc'  => 'required',
                 'HMPCost'  => 'required|numeric',
-             //   'HMPImage' => 'required|mimes:jpg,jpeg,png',
+                'HMPImage' => 'required|mimes:jpg,jpeg,png',
             ], $messages);
 
             $package_update = HomeMakerPackages::where('HMPId', $homemaker_package_id)->update(['HMPName' => request('HMPName'), 'HMPDesc' => request('HMPDesc'), 'HMPCost' => request('HMPCost')]);
 
-/*            if ($request->hasFile('HMPImage')) {
+            if ($request->hasFile('HMPImage')) {
 
                 #$dir=.$request->user()->id;
                 $request->file('HMPImage')->storeAs('public/upload/' . $request->user()->id . '/packages/' . $homemaker_package_id, 'productimage' . '.' . $request->file('HMPImage')->getClientOriginalExtension());
@@ -106,7 +106,7 @@ class HomeMakerPackagesController extends Controller
                 $homemaker = HomeMakerPackages::where('HMPId', $homemaker_package_id)->update(['HMPImage' => $image]);
 
                 return response()->json(['status' => 'success'], 200);
-            }*/
+            }
 
         } catch (\Exception $e) {
             return response()->json($e->errors(), 203);
@@ -171,6 +171,14 @@ class HomeMakerPackagesController extends Controller
 
             $home_maker_packages = User::join('homemaker', 'homemaker.UserId', '=', 'users.id')
                 ->join('homemakerpackages', 'homemakerpackages.HomeMakerId', '=', 'homemaker.HMId')->where('HMPId', $homemaker_package_id)->first();
+            //    dd($home_maker_packages);
+                    
+              $image_file =    \Storage::get('public/upload/'.$home_maker_packages->id.'/packages/'.$homemaker_package_id.'/'.$home_maker_packages->HMPImage);        
+
+
+
+            $home_maker_packages->prod_encoded_img = base64_encode($image_file);
+        //    dd($home_maker_packages->prod_encoded_img);
             $home_maker_packages->hst   =number_format((float)(0.02 * $home_maker_packages->HMPCost), 2, '.', '') ;
             $total = $home_maker_packages->hst + $home_maker_packages->HMPCost;
             $home_maker_packages->total = number_format((float)$total, 2, '.', '');
