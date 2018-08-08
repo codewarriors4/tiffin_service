e70b851cad7d18fd5ffac88111270896876ae636
jaison <jaison.joseph26@gmail.com>
2018-08-08 18:33:26 -0400

    create menu push notifn

--- web/app/Http/Controllers/Api/HomeMakerPackagesController.php
+++ web/app/Http/Controllers/Api/HomeMakerPackagesController.php
@@ -24,7 +24,7 @@ class HomeMakerPackagesController extends Controller
 
             //dd($user_id);
 
-            $messages = array("HMPName.required" => "The Package name is required",
+/*            $messages = array("HMPName.required" => "The Package name is required",
                 "HMPDesc.required"                   => "The package description is required",
                 "HMPCost.required"                   => "The package cost is required",
                 "HMPCost.numeric"                    => "Please enter only numeric value for cost",
@@ -38,7 +38,7 @@ class HomeMakerPackagesController extends Controller
                 'HMPDesc'  => 'required',
                 'HMPCost'  => 'required|numeric',
                 'HMPImage' => 'required|mimes:jpg,jpeg,png',
-            ], $messages);
+            ], $messages);*/
 
             $home_maker = HomeMaker::where('UserId', $user_id)->first();
 
