fcbb3b7a2d9b467d4d366c6e0f842bc3df232add
jaison <jaison.joseph26@gmail.com>
2018-08-08 14:00:51 -0400

    packages added suppport for images

--- web/app/Http/Controllers/Api/HomeMakerPackagesController.php
+++ web/app/Http/Controllers/Api/HomeMakerPackagesController.php
@@ -18,6 +18,8 @@ class HomeMakerPackagesController extends Controller
 
             $user_id = \Auth::user()->id;
 
+            //dd($user_id);
+
             $messages = array("HMPName.required" => "The Package name is required",
                 "HMPDesc.required"                   => "The package description is required",
                 "HMPCost.required"                   => "The package cost is required",
@@ -31,7 +33,7 @@ class HomeMakerPackagesController extends Controller
                 'HMPName'  => 'required',
                 'HMPDesc'  => 'required',
                 'HMPCost'  => 'required|numeric',
-               // 'HMPImage' => 'required|mimes:jpg,jpeg,png',
+                'HMPImage' => 'required|mimes:jpg,jpeg,png',
             ], $messages);
 
             $home_maker = HomeMaker::where('UserId', $user_id)->first();
@@ -57,9 +59,15 @@ class HomeMakerPackagesController extends Controller
 
             }
 
+           // $this->sendFCMOnPackageCreate($home_maker);
+
+            
+
             return response()->json(['status' => 'success'], 200);
 
         } catch (\Exception $e) {
+
+        //    dd($e);
 
             //   return response()->json(['status' => 'successsss'], 203);
 
@@ -68,6 +76,40 @@ class HomeMakerPackagesController extends Controller
         }
 
     }
+
+
+    public function sendFCMOnPackageCreate($homemakerid)
+    {
+
+        $subs_notify      = \Config::get('constants.options.new_package_created');
+        $send_push_notifn = 0; // 0:send notify 1: dont send
+
+        $fcmtokens = UserMobInfo::where('userID', $user_id)->select("fcmtoken")->get();
+
+        $device_token_array = array();
+
+
+         $subs = User::join('tiffinseeker', 'tiffinseeker.UserId', '=', 'users.id')
+                ->join('subscription', 'subscription.TiffinSeekerId', '=', 'tiffinseeker.TSid')
+                ->join('homemakerpackages', 'homemakerpackages.HMPId', '=', 'subscription.HMPid')
+                ->join('homemaker', 'homemaker.HMId', '=', 'subscription.HomeMakerId')
+                ->where('subscription.HomeMakerId', $homemakerid)->orderBy('subscription.created_at', 'desc')->get();
+
+
+        if($subs->count()>0){
+            foreach ($subs as $key => $sub) {
+                
+
+            }
+        }
+
+
+
+
+
+    }
+
+
 
     public function HMPUpdate(Request $request)
     {
--- web/config/constants.php
+++ web/config/constants.php
@@ -5,6 +5,8 @@ return [
         'searchradius' => '20',
         'Admin_approval_notify' =>1,
         'New_susbcriber_notify' =>2,
+        'new_package_created' =>3,
+
 
     ]
 ];
